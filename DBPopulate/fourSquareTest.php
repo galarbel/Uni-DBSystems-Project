a8ff41e19a7da095398640e08e1926ac4e29baeb
Ariel Goldberger <argold82@gmail.com>
2016-12-31 12:50:00 +0200

    :)

--- DBPopulate/fourSquareTest.php
+++ DBPopulate/fourSquareTest.php
@@ -6,7 +6,7 @@ header('Content-type: application/json');
 $url = "https://api.foursquare.com/v2/venues/search";
 
 $client_id = "VVFPKXJBWNMA1KQUJ50KYM5W5AK0IEIBITKTJPKVX3PQQDVR";
-$client_secret = "Q4FNEYLWT0OU0JWQDXEERMKPL1OOZIE0JBXCDTA0A0J05EDN&v=20161231";
+$client_secret = "Q4FNEYLWT0OU0JWQDXEERMKPL1OOZIE0JBXCDTA0A0J05EDN";
 /*-----------------------------*/
 
 /*-------Search Params--------*/
@@ -31,7 +31,7 @@ die;
 */
 
  $ch = curl_init(); 
- curl_setopt($ch, CURLOPT_URL, $url); 
+ curl_setopt($ch, CURLOPT_URL, $apiCallURL); 
  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
  $output = curl_exec($ch); 
