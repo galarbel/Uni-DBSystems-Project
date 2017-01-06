305470d9e13c0b5910b9cb6c25486969c43d5564
Gal <gal.arbel88@gmail.com>
2017-01-06 17:43:39 +0200

    identation...

--- DBPopulate/Scripts/insert_toTempTable.php
+++ DBPopulate/Scripts/insert_toTempTable.php
@@ -7,21 +7,17 @@ include_once '../Global/config.php';
 
 $USA_START_LATITUDE = 31.000000;
 
-for ($latitude =    40.500000; $latitude <= 48.000000; $latitude = $latitude + 0.05 ){
+for ($latitude =    40.600000; $latitude <= 48.000000; $latitude = $latitude + 0.05 ){
     for ($longitude =  -125.000000; $longitude <= -70.000000; $longitude = $longitude + 0.05){
-    
 
-    $results = getVenusFromFourSquare($latitude,$longitude);
+        $results = getVenusFromFourSquare($latitude,$longitude);
 
+        $resultsArr = json_decode($results);
+        $vArr = $resultsArr->response->venues;
 
-
-    $resultsArr = json_decode($results);
-    $vArr = $resultsArr->response->venues;
-
-    //echo "Return Code:" . $resultsArr->meta->code . "<br>";
-    //echo "Venues:" . sizeof($vArr). "<br>";
-    //echo json_encode($vArr[0]);
-
+        //echo "Return Code:" . $resultsArr->meta->code . "<br>";
+        //echo "Venues:" . sizeof($vArr). "<br>";
+        //echo json_encode($vArr[0]);
         foreach ($vArr as $v) {
             $id         = $v->id;
             $name       = $v->name;
@@ -29,9 +25,9 @@ for ($latitude =    40.500000; $latitude <= 48.000000; $latitude = $latitude + 0
             $flongitude = $v->location->lng;
 
             $address        = (isset($v->location->address) ? $v->location->address : null);
-            $city           = (isset($v->location->city) ? $v->location->city : null);
-            $country_code   = (isset($v->location->cc) ? $v->location->cc : null);
-            $state          = (isset($v->location->state) ? $v->location->state : null);
+            $city           = (isset($v->location->city)    ? $v->location->city : null);
+            $country_code   = (isset($v->location->cc)      ? $v->location->cc : null);
+            $state          = (isset($v->location->state)   ? $v->location->state : null);
             $country_name   = (isset($v->location->country) ? $v->location->country : null);
 
             $categories = null;
@@ -46,9 +42,9 @@ for ($latitude =    40.500000; $latitude <= 48.000000; $latitude = $latitude + 0
             //die;
         }
 
-    usleep(50);
-    echo $latitude . ", " . $longitude . " NEXT!<br>";
-    flush();
+        usleep(50);
+        echo $latitude . ", " . $longitude . " NEXT!<br>";
+        flush();
     }
 }
 
