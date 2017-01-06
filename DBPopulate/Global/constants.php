2410bfd502df9e52e0698435376490ec89a5d23f
Gal <gal.arbel88@gmail.com>
2017-01-07 01:03:20 +0200

    quota reached :(

--- DBPopulate/Global/constants.php
+++ DBPopulate/Global/constants.php
@@ -12,8 +12,10 @@ $DBName		= "DbMysql12";
 $fourSquareAPI = "https://api.foursquare.com/v2/venues";
 $fourSquareAPISearch = $fourSquareAPI . "/search";
 
-$fourSquare_client_id = "VVFPKXJBWNMA1KQUJ50KYM5W5AK0IEIBITKTJPKVX3PQQDVR";
-$fourSquare_client_secret = "Q4FNEYLWT0OU0JWQDXEERMKPL1OOZIE0JBXCDTA0A0J05EDN";
+//$fourSquare_client_id = "VVFPKXJBWNMA1KQUJ50KYM5W5AK0IEIBITKTJPKVX3PQQDVR"; //quota reached?
+//$fourSquare_client_secret = "Q4FNEYLWT0OU0JWQDXEERMKPL1OOZIE0JBXCDTA0A0J05EDN"; //quota reached?
+$fourSquare_client_id = "3FLRXLDWATRJ3ZMDA1HXAWOYRC5DMCPPMSB2E1U00K32X1K0";
+$fourSquare_client_secret = "QQ5HQZKQWPYM3X31WIPMZOPBI4HWSFUBEYDDKFT3WSRX50UD";
 
 $fourSquare_v = "20161231";
 $fourSquare_radius = "1000";
