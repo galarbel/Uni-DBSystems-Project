74e3a4eb175e29efd0edcb534ab34387c0018826
Gal <gal.arbel88@gmail.com>
2017-01-14 01:33:41 +0200

    no message

--- Server/WS/get_day_plan.php
+++ Server/WS/get_day_plan.php
@@ -2,15 +2,60 @@
 
 include_once '../Global/config.php';
 
-$sqlQuery = "SELECT place_id, foursquare_id FROM Places limit 5";
-$results = $db->rawQuery($sqlQuery);
+$sqlQuery = "call server_get_by_distance (?, ?, ?)";
 
-foreach ($results as $v) {
-    $id = $v["place_id"];
-    $f_id = $v["foursquare_id"];
+$lati = null;
+$longi = null;
+$max_price = null;
 
-    echo "id: " . $id . ", foursquare_id: " . $f_id ;
-    echo "<br>";
+
+if (isset($_REQUEST["latitude"]) && is_numeric($_REQUEST["latitude"])) {
+    $lati = $_REQUEST["latitude"];
 }
+
+if (isset($_REQUEST["longitude"]) && is_numeric($_REQUEST["longitude"])) {
+    $longi = $_REQUEST["longitude"];
+}
+
+if (isset($_REQUEST["price"]) && is_numeric($_REQUEST["price"])) {
+    $max_price = $_REQUEST["price"];
+}
+
+if (! ($lati && $longi)) {
+    die;
+}
+
+
+/* OK - print JSON */
+
+header('Content-type: application/json');
+
+$return = [];
+
+
+$requestsParams = [$lati, $longi, $max_price];
+$results = $db->rawQuery($sqlQuery, $requestsParams);
+
+//echo json_encode($results); die;
+
+foreach ($results as $p) {
+    switch ($p["category_type"]) {
+        case 1:
+            $return["morning"] = $p;
+            break;
+        case 2:
+            $return["lunch"] = $p;
+            break;
+        case 3:
+            $return["dinner"] = $p;
+            break;
+        case 4:
+            $return["night"] = $p;
+            break;
+    }
+}
+
+
+echo json_encode($return);
 
 ?>
