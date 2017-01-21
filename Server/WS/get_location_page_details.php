19b9bd15705f7c36c586be31421c1dc5781a2723
nitzanh <nitzanh@il.ibm.com>
2017-01-21 14:47:09 +0200

    get details for location page

---
+++ Server/WS/get_location_page_details.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once '../Global/config.php';
+
+if (isset($_REQUEST["place_id"]) ) {
+    $place_id = $_REQUEST["place_id"];
+} else {
+    echo "missing 'place_id' parameter"; die;
+}
+
+header('Content-type: application/json');
+
+$getPlaceDataQuery = "call web_get_place_data_by_id (?)";
+$place_data = $db->rawQuery($getPlaceDataQuery, [$place_id])[0];
+
+$getReviewsQuery = "call web_get_reviews_by_place_id (?)";
+$place_reviews = $db->rawQuery($getReviewsQuery, [$place_id]);
+//print_r($place_reviews);
+$place_data["reviews_count"]= count($place_reviews);
+$place_data["reviews"]= json_encode($place_reviews);
+
+echo json_encode($place_data);
+
+?>
